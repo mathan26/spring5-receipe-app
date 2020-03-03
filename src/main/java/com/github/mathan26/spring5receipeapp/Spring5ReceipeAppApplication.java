223c9e624e8968fa8f6d81c3792322861a51b60d
mathan26 <mathan.se@gmail.com>
2020-03-03 05:55:10 +0530

    inital commit

---
+++ src/main/java/com/github/mathan26/spring5receipeapp/Spring5ReceipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.spring5receipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReceipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5ReceipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mathan26/spring5receipeapp/Spring5ReceipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.spring5receipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ReceipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
