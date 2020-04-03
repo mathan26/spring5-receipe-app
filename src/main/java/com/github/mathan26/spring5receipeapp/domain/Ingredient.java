54ad1f5f81dc500e7a812eb1dc8cb9a23633f419
mathan26 <mathan.se@gmail.com>
2020-04-03 21:47:30 +0530

    Branch : one-to-one-jpa-assign-review 135. Assignment - Review

--- src/main/java/com/github/mathan26/spring5receipeapp/domain/Ingredient.java
+++ src/main/java/com/github/mathan26/spring5receipeapp/domain/Ingredient.java
@@ -12,10 +12,12 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private  UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -47,5 +49,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
---
+++ src/main/java/com/github/mathan26/spring5receipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package com.github.mathan26.spring5receipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
