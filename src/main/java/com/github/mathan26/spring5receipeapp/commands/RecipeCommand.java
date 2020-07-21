305c67940b7d1e4d9a547e560d8795c634de8c3b
mathan26 <mathan.se@gmail.com>
2020-07-21 18:45:09 +0530

    Validation constraint added

--- src/main/java/com/github/mathan26/spring5receipeapp/commands/RecipeCommand.java
+++ src/main/java/com/github/mathan26/spring5receipeapp/commands/RecipeCommand.java
@@ -4,7 +4,9 @@ import com.github.mathan26.spring5receipeapp.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,13 +18,30 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3,max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(999)
     private Integer servings;
+
     private String source;
     private Byte[] image;
+
+    @URL
     private String url;
+
+    @NotNull
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
